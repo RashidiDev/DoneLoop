a70eae1f6f4ec360d2c2c3056e298ab07e0d14af
RashidiDev <alirashidi9997@gmail.com>
2026-08-02 15:30:15 +0330

    Add network status check and notifying the user

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import Header from "./components/header";
 import TasksContainer from "./components/tasks-container";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+import { useOnlineStatus } from "./hooks/useOnlineStatus";
 
 function App() {
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus == true) {
+    toast("You Now Online", {
+      closeButton: true,
+      position: "top-center",
+    });
+  } else {
+    toast("You are Offline", {
+      description: "You can still use the app",
+      position: "top-center",
+    });
+  }
+
   return (
     <div className="App">
       <Header />
       <TasksContainer />
+      <Toaster
+        toastOptions={{
+          classNames: {
+            description: "!text-muted-foreground",
+          },
+        }}
+      />
     </div>
   );
 }
